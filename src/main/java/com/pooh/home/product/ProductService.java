6a2ad48ec04a7f9f1194bff586580e9b78d23621
YU3727 <lacvert3727@naver.com>
2023-01-31 12:21:08 +0900

    230131-3
    servlet

--- src/main/java/com/pooh/home/product/ProductService.java
+++ src/main/java/com/pooh/home/product/ProductService.java
@@ -1,15 +1,53 @@
-package com.pooh.home.test;
+package com.pooh.home.product;
 
 import java.util.List;
 
 import com.pooh.home.member.MemberDAO;
 import com.pooh.home.member.MemberDTO;
-import com.pooh.home.product.ProductDAO;
-import com.pooh.home.product.ProductDTO;
-import com.pooh.home.product.ProductOptionDTO;
 
 public class ProductService { //test용 메서드
 //230130 6교시 Service는 DAO로 작업을 하기 전/후처리를 하기 위한 메서드명이다.
+//230131 1교시 결합도
+	
+	//결합도가 높다(강하다)
+//	1. 변수 생성하면서 초기화(객체생성) - 잘안씀
+	private ProductDAO pDAO; //= new ProductDAO(); - 잘안씀
+	
+//	2. instance block - 잘안씀
+	{
+		this.pDAO = new ProductDAO();
+	}
+//	3. 생성자에서 초기화(객체생성)
+	public ProductService() { 
+		this.pDAO = new ProductDAO();
+	}
+	
+	//결합도가 낮다(약하다)
+//	4. setter/getter 메서드 사용
+	public void setProductDAO(ProductDAO pDAO) {
+		this.pDAO = pDAO;
+	}
+	
+	
+	public int setAddProduct(ProductDTO pDTO, List<ProductOptionDTO> ar) throws Exception{
+		//다른클래스의 메서드와 이름이 중복되어도 상관x 다른클래스니까
+		//product table에 상품을 등록, option도 등록
+		//입력을 다른곳에서 받아올거기 때문에 매개변수가 필요함
+		//호출하려면 객체부터 만들어야한다는걸 생각하자
+		Long productNum = pDAO.getProductNum();
+		pDTO.setProductNum(productNum); //pDTO에는 시퀀스번호가 들어가있지 않으므로 넣어준다.
+		int result = pDAO.setAddProduct(pDTO);
+		
+		//옵션은 반복해야 하므로 반복문을 돌린다. List에서 꺼낸건 ProductOptionDTO이므로
+		for(ProductOptionDTO poDTO : ar) {
+			poDTO.setOptionNum(productNum);
+			result = pDAO.setAddProductOption(poDTO); //옵션은 최소 1개 이상이므로 List를 사용한다
+		}
+		
+		return result;
+		
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
